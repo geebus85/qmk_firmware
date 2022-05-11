8c6ca9e8714fb371b416f0b3aa00b89145e2cef7
geebus85 <105387323+geebus85@users.noreply.github.com>
2022-05-11 13:11:26 -0700

    test

--- keyboards/sofle/keymaps/garry/keymap.c
+++ keyboards/sofle/keymaps/garry/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                           KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,   KC_F11, \
-  _______, KC_INS,  KC_UP, KC_APP,  XXXXXXX,  XXXXXXX,                         KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC, \
+  _______, KC_INS,  KC_UP, RGB_TOG,  RGB_RMOD,  RGB_MOD,                         KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC, \
   _______, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,  KC_CAPS,                         KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC, \
   _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX, _______,       _______, XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,  XXXXXXX,  _______, \
                     _______, _______, _______,  _______, _______,       _______, _______, _______,  _______, _______ \
